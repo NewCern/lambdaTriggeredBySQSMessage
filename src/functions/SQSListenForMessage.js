13d842e0acc5ed73cfdc6c3bbb0cd7f9446b7ee0
NewCern <lwiltshire@galaxe.com>
2023-05-23 17:18:03 -0400

    update payload reception

--- src/functions/SQSListenForMessage.js
+++ src/functions/SQSListenForMessage.js
@@ -10,40 +10,64 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
-const aws_sdk_1 = require("aws-sdk");
+const xml2js_1 = require("xml2js");
 const handler = (event) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        // Process each SQS record in the event
+        // Process each array item in the queue
         for (const record of event.Records) {
-            // Retrieve the message body
+            // isolate message object
             const { body } = record;
-            // Logic to seperate .xml to individual files will go here
-            // Delete the message from the queue
-            //   await deleteMessage(record);
+            // convert to javascript object
+            const message = JSON.parse(body);
+            // extract key, which contains xml file
+            const xml = message.key;
+            // convert to buffer
+            const xmlBuffer = Buffer.from(xml);
+            // then to string
+            const xmlBufferToString = xmlBuffer.toString();
+            console.log("Buffer to String result: ", xmlBufferToString);
+            // const options = {
+            //   explicitArray: false,
+            // };
+            // const jsonObjects = await parseXML(xmlBufferToString, options);
+            // console.log("Here is the parseXML being invoked: ", jsonObjects);
         }
     }
     catch (error) {
-        console.error('Error processing SQS messages:', error);
+        console.error('Error processing SQS messages in the catch:', error);
         throw error;
     }
 });
 exports.handler = handler;
-function deleteMessage(record) {
+function parseXML(xmlFile, options) {
     return __awaiter(this, void 0, void 0, function* () {
-        const { receiptHandle } = record;
-        const queueUrl = "https://sqs.us-east-1.amazonaws.com/233784350905/xml-file-added-to-queue";
-        const sqs = new aws_sdk_1.SQS();
-        const params = {
-            QueueUrl: queueUrl,
-            ReceiptHandle: receiptHandle,
-        };
-        try {
-            yield sqs.deleteMessage(params).promise();
-            console.log('Deleted SQS message:', record.messageId);
-        }
-        catch (error) {
-            console.error('Error deleting SQS message:', error);
-            throw error;
-        }
+        return new Promise((resolve, reject) => {
+            (0, xml2js_1.parseString)(xmlFile, options, (error, result) => {
+                // if (error) {
+                //   console.log("There was an error: ", error);
+                //   reject(error);
+                // } else {
+                const json = JSON.stringify(result, null, 1);
+                resolve(json);
+                console.log("Here is the json: ", json);
+                // }
+            });
+        });
     });
 }
+// async function deleteMessage(record: SQSRecord) {
+//   const { receiptHandle } = record;
+//   const queueUrl = "https://sqs.us-east-1.amazonaws.com/233784350905/xml-file-added-to-queue";
+//   const sqs = new SQS();
+//   const params = {
+//     QueueUrl: queueUrl,
+//     ReceiptHandle: receiptHandle,
+//   };
+//   try {
+//     await sqs.deleteMessage(params).promise();
+//     console.log('Deleted SQS message:', record.messageId);
+//   } catch (error) {
+//     console.error('Error deleting SQS message:', error);
+//     throw error;
+//   }
+// }
