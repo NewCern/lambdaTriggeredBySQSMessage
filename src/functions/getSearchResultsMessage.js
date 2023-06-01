eb5b982c953787f3f1f068620f6a02d088c83b05
NewCern <lwiltshire@galaxe.com>
2023-06-01 19:35:19 -0400

    updates

--- src/functions/getSearchResultsMessage.js
+++ src/functions/getSearchResultsMessage.js
@@ -17,7 +17,7 @@ exports.handler = void 0;
 const client_sqs_1 = require("@aws-sdk/client-sqs");
 const sqs = new client_sqs_1.SQSClient({ region: "us-east-1" });
 const queueUrl = "https://sqs.us-east-1.amazonaws.com/233784350905/search-results-objects-queue";
-const handler = () => __awaiter(void 0, void 0, void 0, function* () {
+const handler = (event) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         // // Check if the request method is GET
         // if (event.httpMethod !== 'GET') {
@@ -40,8 +40,8 @@ const handler = () => __awaiter(void 0, void 0, void 0, function* () {
             };
         }
         const message = Messages[0];
-        // console.log("This is Message[0]: ", message)
-        // const { Body: messageBody, ReceiptHandle } = message;
+        console.log("This is Message[0]: ", message);
+        const { Body: messageBody, ReceiptHandle } = message;
         // // // Process the message payload
         // const messageToJson = JSON.parse(messageBody || '{}');
         // const messagePayload = messageToJson.data;
@@ -56,11 +56,9 @@ const handler = () => __awaiter(void 0, void 0, void 0, function* () {
         const response = {
             'statusCode': 200,
             'headers': {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Methods": "*",
-                "Accept": '*/*',
-                "Content-Type": 'application/json'
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
             },
             body: JSON.stringify("bodyParsed"),
         };
--- src/functions/getSearchResultsMessage.ts
+++ src/functions/getSearchResultsMessage.ts
@@ -47,7 +47,7 @@ import { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } from "@aws-sdk
 const sqs = new SQSClient({ region: "us-east-1" });
 const queueUrl = "https://sqs.us-east-1.amazonaws.com/233784350905/search-results-objects-queue";
 
-export const handler = async (): Promise<any> => {
+export const handler = async (event: APIGatewayProxyResult): Promise<APIGatewayProxyResult> => {
   try {
     // // Check if the request method is GET
     // if (event.httpMethod !== 'GET') {
@@ -74,8 +74,8 @@ export const handler = async (): Promise<any> => {
     }
 
     const message = Messages[0];
-    // console.log("This is Message[0]: ", message)
-    // const { Body: messageBody, ReceiptHandle } = message;
+    console.log("This is Message[0]: ", message)
+    const { Body: messageBody, ReceiptHandle } = message;
 
     // // // Process the message payload
     // const messageToJson = JSON.parse(messageBody || '{}');
@@ -93,11 +93,9 @@ export const handler = async (): Promise<any> => {
     const response: APIGatewayProxyResult = {
       'statusCode': 200,
       'headers': {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Accept": '*/*',
-        "Content-Type": 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify("bodyParsed"),
     };
